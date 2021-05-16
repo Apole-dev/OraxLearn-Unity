1270f9cdfaf1008dffc7d9c854e3a6992778c2cb
Eren <74319360+Apole-dev@users.noreply.github.com>
2021-05-16 14:14:49 +0300

    Orax Learn Unity Lessons
    For more lessons  https://Orax.net

---
+++ [Unity] Lessons/Lessons-1-DebugLog/DebugLog.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace OraxLearnUnity.DebugLog
+{
+    public class DebugLog : MonoBehaviour
+    {
+        private string _DebugLogTextStart = "Orax.net";
+        private string _DebugLogTextUpdate = "Test";
+        void Start() => Debug.Log($"<color=red>Color Red {_DebugLogTextStart} :</color>");
+
+        void Update() => Debug.Log(_DebugLogTextUpdate);
+
+         private void test( ) {
+
+
+           Debug.Log($"<color=red>Color Red {_DebugLogTextStart} :</color>");
+
+
+        }
+
+        
+    }
+}
+
